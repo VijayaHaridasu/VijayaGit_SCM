cd3eee669fb17a18157a6795d5ac18d15aa02766
naga bellamkonda <vijaya.haridasu@gmail.com>
2023-02-21 12:34:23 +1100

    newchange

---
+++ src/test/java/Login/AT01_Navigatetohomepage.java
@@ -0,0 +1,14 @@
+package Login;
+
+import org.junit.Test;
+
+import java.lang.annotation.Target;
+
+public class AT01_Navigatetohomepage {
+    @Test
+    public void Navigate()
+    {
+        System.out.println("Navigate to the homepage");
+    }
+
+}
