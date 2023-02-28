492243548f32341e3c38b38b0981e6c62c5c25ec
naga bellamkonda <vijaya.haridasu@gmail.com>
2023-02-28 20:23:04 +1100

    log

---
+++ src/test/java/StepDefinitions/AT02_login.java
@@ -0,0 +1,12 @@
+package StepDefinitions;
+
+import org.junit.Test;
+
+public class AT02_login {
+    @Test
+    public void login()
+    {
+        System.out.println("this is login page");
+
+    }
+}
